8f6c4781dd27c9b6cd7f92b870e1577090edec88
Azir@9200 <aziruddin83@hotmail.com>
2024-09-30 14:23:20 +0200

    make payment successful

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,56 @@
-
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <footer className="bg-gray-800 text-white py-8">
+      <div className="container mx-auto flex flex-col items-center">
+        <div className="flex space-x-4 mb-4">
+          <a href="/" className="hover:underline">
+            Home
+          </a>
+          <a href="/services" className="hover:underline">
+            Services
+          </a>
+          <a href="/about" className="hover:underline">
+            About Us
+          </a>
+          <a href="/contact" className="hover:underline">
+            Contact
+          </a>
         </div>
-    );
+        <div className="mb-4">
+          <p>
+            &copy; {new Date().getFullYear()} Car Wash Booking System. All
+            Rights Reserved.
+          </p>
+        </div>
+        <div className="flex space-x-4">
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-500"
+          >
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-400"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-pink-500"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
